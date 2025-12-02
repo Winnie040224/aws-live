3e5ab6ef7b0d34f92ee57987dc8b2fc74522e2e0
TAN ZHEN XIANG <tanzx-wm22@student.tarc.edu.my>
2025-12-02 11:07:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cox4m8ggqwl0.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "tanzhenxiang-employee"
+customregion = "us-east-1"
+
